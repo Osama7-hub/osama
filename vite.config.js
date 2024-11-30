bfebb5858f143580790698fb70e9116f2ae499e0
Osama Ahmed <osama.des795@gmail.com>
2024-11-30 17:53:38 +0200

    edit config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+},
 })
