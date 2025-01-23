736f91a87b5237fed202590f2554c4387b343f9f
Osama Ahmed <osama.des795@gmail.com>
2025-01-23 15:00:17 +0200

    edit cv

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,14 +20,17 @@ function Hero() {
                 <Row>
                     <Col md={12} lg={6}>
                         <div className='hero-info'>
-                            <img src="./imgs/Osama.jpg" alt="avatar" width={100} height={100} className='avatar rounded-circle' />
+                            <img src="./imgs/Osama.jpg" alt="avatar" width={100} height={100} className='rounded-circle avatar' />
                             <h2>{translations[language].name} <span> {translations[language].jobTitle}</span></h2>
                             <p>{translations[language].sammary}</p>
-                            <div className="social-icon d-flex align-items-center gap-3">
+                            <div className="d-flex align-items-center gap-3 social-icon">
                                 <a href="https://wa.me/97433467648" target="_blank" rel="noopener noreferrer"><img src="./imgs/whatsapp_icon.png" alt="whatsapp-icon" width={25} height={25} /></a>
                                 <a href="https://www.linkedin.com/authwall?trk=bf&trkInfo=AQEeRDdEMvwb0gAAAZL8fELYdt2m3P_ABa04W1hjHErbt-JKyu_SHKgoLYci6pQF6ukcyhLhviq7uA_L186RVpZGs0jYhBGC92aL4z0Q8Ugxsc5FZsA0IdY6w2gSUX1lFwUBd4M=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fosama-ahmed-86b455208%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_app" target="_blank" rel="noopener noreferrer"><img src="./imgs/linkedIn-icon.png" alt="linkedIn-icon" width={25} height={25} /></a>
                                 <a href="https://github.com/Osama7-hub" target="_blank" rel="noopener noreferrer"><img src="./imgs/github.png" alt="github-icon" width={25} height={25} /></a>
-                                <a href="files/Osama-cv.pdf" className='btn btn-primary'><FontAwesomeIcon icon={faFilePdf} /> {translations[language].cvButton}</a>
+                                {
+                                    language === "ar" ? <a href="files/اسامة-cv.pdf" className='btn btn-primary'><FontAwesomeIcon icon={faFilePdf} /> {translations[language].cvButton}</a>
+                                        : <a href="files/Osama-cv.pdf" className='btn btn-primary'><FontAwesomeIcon icon={faFilePdf} /> {translations[language].cvButton}</a>
+                                }
                             </div>
                         </div>
                     </Col>
